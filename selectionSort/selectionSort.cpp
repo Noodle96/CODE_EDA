66a195c64d68f11a1e13a4cf68a743277159fa60
Noodle96 <jorgealfredomarino2@gmail.com>
2021-09-28 00:52:31 -0500

    add shellSort.cpp

--- selectionSort/selectionSort.cpp
+++ selectionSort/selectionSort.cpp
@@ -34,35 +34,11 @@ auto timeSelectionSort(int arr[],int len){
     return duration;
 }
 
-/* Function to print an array */
-void printArray(int arr[], int size){
-    for (int i=0; i < size; i++)
-        cout << arr[i] << " ";
-    cout << endl;
-}
-
-// Driver program to test above functions
 int main(){
 
     int TOTAL = 1000000;
     int arr[TOTAL+1];
     cout << "hello" << endl;
-    // for(int e = 1000000 ; e < 5000001; e+=1000000){
-    //
-    // }
-
-
-    // int cont = 0;
-    // string line;
-    // ifstream myfile ("../filenumbers.txt");
-    // if (myfile.is_open()) {
-    //     while ( getline (myfile,line) ){
-    //         //cout << line << '\n';
-    //         cont++;
-    //     }
-    //     myfile.close();
-    // }else{cout << "error al abrir0";}
-    // std::cout << "constador: "<<cont << std::endl;
 
     std::ofstream dataSelectionSort;
     dataSelectionSort.open("selectionSortCPP.txt",std::ios::trunc);
@@ -72,44 +48,16 @@ int main(){
         ifstream numeros ("../numeros.txt");
         if (numeros.is_open()) {
             while ( getline (numeros,line) && contador < intervalo ){
-                // for(int num = 0 ; num < intervalo; num++){
-                //     arr[num] = convertToInt(line);
-                // }
-                //cout << line << endl;
                 arr[contador] = convertToInt(line);
                 contador++;
             }
         numeros.close();
 
-        //printing
-        // for(int num = 0 ; num < 10; num++)
-        //     std::cout << arr[num] << endl;
-        // }std::cout << endl;
-
         //AGREGANDO A selectionSortCPP.txt
         auto time = timeSelectionSort(arr,intervalo);
-        //cout << "Time: "<< time.count() << endl;
         dataSelectionSort << intervalo << "," << (time.count()/1000000) << endl;
 
         }
     }
-
-
-
-
-
-    // int arr[] = {64, 25, 12, 22, 11};
-    // int n = sizeof(arr)/sizeof(arr[0]);
-    // selectionSort(arr, n);
-    // cout << "Sorted array: \n";
-    // printArray(arr, n);
-
-
-
-
-
-
     return 0;
 }
-
-// This is code is contributed by rathbhupendra
---
+++ shellSort/shellSort.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <fstream>
+#include<chrono>
+using namespace std;
+using namespace std::chrono;
+
+int convertToInt(string line){
+    return stoi(line);
+}
+
+// Shell sort
+void shellSort(int array[], int n) {
+  // Rearrange elements at each n/2, n/4, n/8, ... intervals
+  for (int interval = n / 2; interval > 0; interval /= 2) {
+    for (int i = interval; i < n; i += 1) {
+      int temp = array[i];
+      int j;
+      for (j = i; j >= interval && array[j - interval] > temp; j -= interval) {
+        array[j] = array[j - interval];
+      }
+      array[j] = temp;
+    }
+  }
+}
+
+auto timeShellSort(int arr[],int len){
+    auto start = high_resolution_clock::now();
+    shellSort(arr,len);
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop - start);
+    return duration;
+}
+
+
+// Driver code
+int main() {
+    int TOTAL = 500000;
+    int arr[TOTAL+1];
+    cout << "hello" << endl;
+
+    std::ofstream dataShellSort;
+    dataShellSort.open("shellSortCPP.txt",std::ios::trunc);
+    for(int intervalo = 100000 ; intervalo < TOTAL+1; intervalo+=200000){
+        int contador = 0;
+        string line;
+        ifstream numeros ("../numeros.txt");
+        if (numeros.is_open()) {
+            while ( getline (numeros,line) && contador < intervalo ){
+                arr[contador] = convertToInt(line);
+                contador++;
+            }
+        numeros.close();
+
+        auto time = timeShellSort(arr,intervalo);
+        dataShellSort << intervalo << "," << (time.count()) << endl;
+        }
+    }
+    dataShellSort.close();
+}
